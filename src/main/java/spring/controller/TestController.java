b26469049171fda200477730c3d287f08e759448
Win Khant Naing <winkhantnaing2003@gmail.com>
2024-04-28 15:04:22 +0630

    Add Project

---
+++ src/main/java/spring/controller/TestController.java
@@ -0,0 +1,13 @@
+package spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping(value = "/")
+	public String home() {
+		return "index";
+	}
+}
