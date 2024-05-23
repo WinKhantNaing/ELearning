642a2ca0542517d90845d8e0d7ea4afbd226e474
minhtetsoe1 <minhtetsoe186@gmail.com>
2024-05-23 22:59:12 +0630

    update for adding

--- src/main/webapp/resources/js/subPlan.js
+++ src/main/webapp/resources/js/subPlan.js
@@ -21,8 +21,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 "1 year", "2 years", "3 years", "4 years", "5 years", "6 years", 
                 "7 years", "8 years", "9 years", "10 years"
             ];
-        }else{
-        	options = [ "1 day", "3 days", "7 days","10 days", "15 days" , "20 days"]
         }
         
 
